baba20f678d84724ece564bd70732ceca81a7efa
zailyapov <115218244+zailyapov@users.noreply.github.com>
2022-10-14 16:03:21 +0600

    Add files via upload

---
+++ DZ4_Z.Bayel_19-01/app.js
@@ -0,0 +1,9 @@
+// // обработчик собития
+// function buttonClick(){
+//     const input = document.getElementById('input');
+//     alert(input.value);
+// }
+// задание 2
+let month = ["Январь", "Февраль", "Март", 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    alert( month[9] );
+    console.log(month);
